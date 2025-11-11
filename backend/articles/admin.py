50bd8c78f356eaf2173aad0f2ade95425b3dcedf
Reinald Van Praet <reinald.vanpraet@gmail.com>
2025-11-11 11:52:55 +0100

    BE: Added articles to admin

--- backend/articles/admin.py
+++ backend/articles/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Article
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'created_at')
+    
+# Register your models here.
+admin.site.register(Article, ArticleAdmin)
--- backend/articles/models.py
+++ backend/articles/models.py
@@ -5,3 +5,6 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title

8aeca43411b043a0608a6658b179c04a57c440c8
Reinald Van Praet <reinald.vanpraet@gmail.com>
2025-11-11 12:23:30 +0100

    BE: Fix gitignore for backend

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -122,3 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Whitelists for CORS
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:5173' # Vite dev server
+]
